88ed4ed9bf3892edc6076d0ad43fc61f471d12ca
Murillo Santana <mvbentosantana@gmail.com>
2026-09-26 10:21:43 +0200

    good enough for now

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,28 @@ int main(void)
 		//data
 		ray_marcher::rasterizer::Scene scene;
 		ray_marcher::rasterizer::Plane* plane = new ray_marcher::rasterizer::Plane;
-		ray_marcher::rasterizer::Sphere* sphere = new ray_marcher::rasterizer::Sphere;
+		ray_marcher::rasterizer::Sphere* spheres[] = {
+			new ray_marcher::rasterizer::Sphere, new ray_marcher::rasterizer::Sphere, new ray_marcher::rasterizer::Sphere
+		};
 		//plane
 		plane->m_point = {0, -1, 0};
 		plane->m_normal = {0, +1, 0};
 		plane->m_material.m_color = {1, 0, 0};
 		//sphere
-		sphere->m_radius = 0.1;
-		sphere->m_center = {0, 0, 0};
-		sphere->m_material.m_color = {0, 1, 0};
+		spheres[0]->m_radius = 0.1;
+		spheres[1]->m_radius = 0.1;
+		spheres[2]->m_radius = 0.1;
+		spheres[0]->m_center = {-0.4, 0, 0};
+		spheres[1]->m_center = {+0.0, 0, 0};
+		spheres[2]->m_center = {+0.4, 0, 0};
+		spheres[0]->m_material.m_color = {0, 1, 0};
+		spheres[1]->m_material.m_color = {0, 1, 0};
+		spheres[2]->m_material.m_color = {0, 1, 0};
 		//objects
 		scene.m_objects.push_back(plane);
-		scene.m_objects.push_back(sphere);
+		scene.m_objects.push_back(spheres[0]);
+		scene.m_objects.push_back(spheres[1]);
+		scene.m_objects.push_back(spheres[2]);
 		//scene
 		scene.setup();
 		scene.draw();
